3b849c457b713025042323ac6a742624a45c1bec
Johanes-Leyran <102525247+Johanes-Leyran@users.noreply.github.com>
2024-01-13 01:12:07 +0800

    add custom user

---
+++ one_tap_check/accounts/managers.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class OneTapUserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        email = self.normalize_email(email=email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+    
+    def create_studentuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_student", True)
+        
+        return self.create_user(email, password, **extra_fields)
+    
+    def create_staffuser(self, email, password=None, is_teacher=False, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_teacher", is_teacher)
+        
+        return self.create_user(email, password, **extra_fields)
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_teacher", True)
+        extra_fields.setdefault("is_student", True)
+        
+        if extra_fields['is_staff'] is not True:
+            raise ValueError("Superuser must be is_staff=True")
+        if extra_fields['is_superuser'] is not True:
+            raise ValueError("Superuser must be is_superuser=True")
+
+        return self.create_user(email, password, **extra_fields)
--- one_tap_check/accounts/models.py
+++ one_tap_check/accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+import uuid
 
-# Create your models here.
+
+class OneTapUser(AbstractBaseUser, PermissionsMixin):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    email = models.EmailField('email address', unique=True)
+    first_name = models.CharField(default="", max_length=120)
+    last_name = models.CharField(default="", max_length=120)
+    
+    is_staff = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
