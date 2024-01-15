4219a443e1c99a5dc6e0d967808c52c00d91af3c
Johanes-Leyran <102525247+Johanes-Leyran@users.noreply.github.com>
2024-01-15 15:11:25 +0800

    s

--- one_tap_check/attendances/views.py
+++ one_tap_check/attendances/views.py
@@ -6,4 +6,4 @@ from .models import Attendance
 def attendance_detail(request, attendance_id):
     attendance = Attendance.objects.get(id=attendance_id)
     
-    return render(request, 'attendance_detail.html', {'attendance': attendance})
+    return render(request, 'room101.html', {'attendance': attendance})
