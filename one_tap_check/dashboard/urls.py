ef9d32d9c5a8cca8e7152fc813e8adb3d694ccc7
Johanes-Leyran <102525247+Johanes-Leyran@users.noreply.github.com>
2024-04-18 04:49:21 +0800

    major update

--- one_tap_check/dashboard/urls.py
+++ one_tap_check/dashboard/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('teacher/<str:pk>', views.dashboard_teacher, name='teacher_dashboard'),
     path('teacher/<str:pk>/section/list/', views.section_list, name='section_list'),
-    path('teacher/<str:pk>/section/view/<str:name>', views.section_view, name='ection_view'),
+    path('teacher/<str:pk>/section/view/<str:name>', views.section_view, name='section_view'),
     path('teacher/<str:pk>/schedules/list', views.schedules, name='schedules_list'),
     path('teacher/<str:pk>/attendance/list/<str:section>', views.attendance_list, name='attendance_list'),
     path('teacher/<str:pk>/attendance/view/<str:at_pk>', views.attendance, name="attendance_view"),
