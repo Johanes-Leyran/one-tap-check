7a05bd8fa34511d3f9a890491d899d6ba62c244a
Johanes-Leyran <102525247+Johanes-Leyran@users.noreply.github.com>
2024-04-18 04:46:38 +0800

    major update

--- one_tap_check/dashboard/urls.py
+++ one_tap_check/dashboard/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('teacher/<str:pk>', views.dashboard_teacher, name='teacher_dashboard'),
-    path('teacher/<str:pk>/section/list/', views.section_list, name='teacher_section_list'),
-    path('teacher/<str:pk>/section/view/<str:name>', views.section_view, name='teacher_section_view'),
-    path('teacher/<str:pk>/schedules/list', views.schedules, name='teacher_schedules_list'),
-    path('teacher/<str:pk>/attendance/list/<str:section>', views.attendance_list, name='teacher_attendance_list'),
-    path('teacher/<str:pk>/attendance/view/<str:at_pk>', views.attendance, name="teacher_attendance_view"),
+    path('teacher/<str:pk>/section/list/', views.section_list, name='section_list'),
+    path('teacher/<str:pk>/section/view/<str:name>', views.section_view, name='ection_view'),
+    path('teacher/<str:pk>/schedules/list', views.schedules, name='schedules_list'),
+    path('teacher/<str:pk>/attendance/list/<str:section>', views.attendance_list, name='attendance_list'),
+    path('teacher/<str:pk>/attendance/view/<str:at_pk>', views.attendance, name="attendance_view"),
 ]
